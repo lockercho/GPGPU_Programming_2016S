26daa59a314e7675d5f821577497f814b0a5d0ec
Pei-Ya Chiu <lockercho@gmail.com>
2017-03-29 22:33:49 +0800

    add const

---
+++ lab1/const.h
@@ -0,0 +1,12 @@
+#ifndef _CONST_H_
+#define _CONST_H_
+
+#define G 0.000000667408
+#define WIDTH 640
+#define HEIGHT 480
+#define W_SIZE 30
+#define H_SIZE 20
+#define FPS 20
+#define SECONDS 6 
+
+#endif
