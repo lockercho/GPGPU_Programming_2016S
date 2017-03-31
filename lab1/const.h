7b1d1b719454c4ff85ebdafa8c642fe21b2ef496
Pei-Ya Chiu <lockercho@gmail.com>
2017-03-31 22:06:54 +0800

    run

--- lab1/const.h
+++ lab1/const.h
@@ -2,11 +2,11 @@
 #define _CONST_H_
 
 #define G 0.000667408
-#define WIDTH 640
-#define HEIGHT 480
+#define WIDTH 1280
+#define HEIGHT 960
 #define W_SIZE 90
 #define H_SIZE 60
-#define FPS 20
+#define FPS 3
 #define SECONDS 3
 
 #endif
