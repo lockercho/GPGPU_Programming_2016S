87d0995a0904170898d7a4d0fb5e82977f633871
Pei-Ya Chiu <lockercho@gmail.com>
2017-04-29 21:41:35 +0800

    Done part 1

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -1,10 +1,12 @@
 #include "counting.h"
 #include <cstdio>
 #include <cassert>
+#include <string>
 #include <thrust/scan.h>
 #include <thrust/transform.h>
 #include <thrust/functional.h>
 #include <thrust/device_ptr.h>
+#include <thrust/host_vector.h>
 #include <thrust/execution_policy.h>
 
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
@@ -12,8 +14,27 @@ __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
 void CountPosition1(const char *text, int *pos, int text_size)
 {
+    std::cerr << "textsize: " << text_size << std::endl;
+    // wrap with a device_ptr 
+    thrust::device_ptr<int> dev_ptr(pos);
+    thrust::device_ptr<const char> txt_ptr = thrust::device_pointer_cast(text);
+//    thrust::fill(dev_ptr, dev_ptr + text_size, (int) 0);
+    
+    int begin =0, end = 0;
+    for(int i=0 ; i < text_size ; i++) {
+        if(txt_ptr[i] != '\n') {
+            end++;
+        } else {
+            if(begin != end) 
+                thrust::sequence(dev_ptr + begin, dev_ptr + end, 1);
+            begin = end = end+1;
+        }
+    }
+    if(begin != end) thrust::sequence(dev_ptr + begin, dev_ptr + end, 1);
+    pos = thrust::raw_pointer_cast(dev_ptr);
 }
 
 void CountPosition2(const char *text, int *pos, int text_size)
 {
 }
+
